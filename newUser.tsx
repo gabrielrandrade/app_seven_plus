e67005e9462056cc71a6081868632d9d51b50c1e
gabrielrandrade <gabrielrandradesp@hotmail.com>
2024-11-28 11:40:49 -0300

    telas login,cadastro,recuperar senha

---
+++ newUser.tsx
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import { StyleSheet,Text, TextInput, Image, View, SafeAreaView, TouchableOpacity,ImageBackground } from 'react-native';
+import styles from './assets/style/style-seven';
+
+
+class App extends Component{
+  render(){
+    return(
+      <SafeAreaView style={styles.container}>
+        <ImageBackground source={require('./assets/image/fundo.jpg')} style={styles.backgroundImage} resizeMode="cover">
+            <View>
+              <Image 
+              source={require('./assets/image/logo.png')} style={styles.logo_login}
+              />
+            </View>
+          <View style={styles.containerLogin}>
+            <View style={styles.containerInput}>
+              <View>
+                <Text style={styles.textLogin}>Criar Usuario</Text>
+                <Text style={styles.text}>Email</Text>
+                <TextInput placeholder='Digite seu email' style={styles.inputLogin}  />
+              </View>
+              <View>
+                  <Text style={styles.text}>Nome</Text>
+                  <TextInput placeholder='Digite a sua senha' style={styles.inputLogin} />
+              </View>
+              <View>
+                  <Text style={styles.text}>Senha</Text>
+                  <TextInput placeholder='Digite a sua senha' style={styles.inputLogin} />
+              </View> 
+              <View>
+                  <Text style={styles.text}>Cofirmar senha</Text>
+                  <TextInput placeholder='Digite a sua senha' style={styles.inputLogin} />
+              </View> 
+            </View>
+            <View>
+                <TouchableOpacity style={styles.buttonLogin}>
+                  <Text>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground> 
+      </SafeAreaView>
+    );
+  }
+
+}
+
+export default App;
